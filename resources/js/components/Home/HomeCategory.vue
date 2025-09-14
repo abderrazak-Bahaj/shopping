76da13133b2e004409b9a50c0dda95cdbed26b86
ba-monarkit <bahaj@monarkit.net>
2025-09-14 23:28:43 +0100

    EIT DEPENDENSES

--- resources/js/components/Home/HomeCategory.vue
+++ resources/js/components/Home/HomeCategory.vue
@@ -2,7 +2,9 @@
   <div class="container py-5">
     <div class="row text-center pt-3">
       <div class="col-lg-6 m-auto">
-        <h1 class="h1 wow fadeInDown" data-wow-delay="0.4s">Categories of The Month</h1>
+        <h1 class="h1 wow fadeInDown" data-wow-delay="0.4s">
+          Categories of The Month
+        </h1>
         <p class="wow fadeInDown" data-wow-delay="5s">
           Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
           officia deserunt mollit anim id est laborum.
@@ -11,15 +13,21 @@
     </div>
     <div class="row">
       <div
-        class="col-12 col-md-4 p-5 mt-3 text-center wow fadeInUp" data-wow-delay="0.5" data-wow-duration="1s"
+        class="col-12 col-md-4 p-5 mt-3 text-center wow fadeInUp"
+        data-wow-delay="0.5"
+        data-wow-duration="1s"
         v-for="category in getAllCategorais.slice(0, 3)"
         :key="category.id"
       >
         <img
           :src="category.cat_img"
-          class="rounded-circle img-fluid border cls-imgs-category"
+          class="rounded-circle img-fluid border cls-imgs-category d-bolck"
         />
-        <router-link :to="`shope/category/${category.id}`" class="text-center mt-3 mb-3 btn btn-secondary"  v-text="category.cat_name"></router-link>
+        <router-link
+          :to="`shope/category/${category.id}`"
+          class="text-center mt-3 mb-3 btn btn-secondary"
+          v-text="category.cat_name"
+        ></router-link>
       </div>
     </div>
   </div>
@@ -34,9 +42,10 @@ export default {
 };
 </script>
 
-<style >
+<style>
 .cls-imgs-category {
-  width: 13em ;
-  height: 13em ;
+  width: 13em;
+  height: 13em;
+  display: block;
 }
 </style>
